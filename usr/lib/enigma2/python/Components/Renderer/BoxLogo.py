18bc1a93399523e974c2cb4dab98965d92e9ef19
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2023-10-13 13:06:53 +0300

    - fix some skin elements - added brand logo to BoxLogo renderer - changed default logo images

--- usr/lib/enigma2/python/Components/Renderer/BoxLogo.py
+++ usr/lib/enigma2/python/Components/Renderer/BoxLogo.py
@@ -14,25 +14,50 @@ except:
 class BoxLogo(Renderer):
 	def __init__(self):
 		Renderer.__init__(self)
-		self.width = 302
-		self.height = 60
-		defaultPngName = resolveFilename(SCOPE_GUISKIN, "icons/logos/deflogo.svg")
-		is_svg = defaultPngName.endswith(".svg")
-		self.defaultLogo = LoadPixmap(defaultPngName, width=self.width, height=0 if is_svg else self.height)
+		self.logoType = "model"
 		
 	GUI_WIDGET = ePixmap
+
+	def applySkin(self, desktop, parent):
+		attribs = self.skinAttributes[:]
+		for (attrib, value) in self.skinAttributes:
+			if attrib == "logoType":
+				self.logoType = value
+				attribs.remove((attrib, value))
+				break
+		self.skinAttributes = attribs
+		return Renderer.applySkin(self, desktop, parent)
 
 	def changed(self, what):
 		pass
 				
 	def onShow(self):
-		print("[BoxLogo] show")
 		if self.instance:
-			print("[BoxLogo] model: " + model)
-			pngname = resolveFilename(SCOPE_GUISKIN, "icons/logos/" + model + ".svg")
+			pngname = resolveFilename(SCOPE_GUISKIN, "icons/logos/" + self.getLogoPngName() + ".svg")
 			is_svg = pngname.endswith(".svg")
-			png = LoadPixmap(pngname, width=self.width, height=0 if is_svg else self.height)
+			png = LoadPixmap(pngname, width=self.instance.size().width(), height=0 if is_svg else self.instance.size().height())
 			if png != None:
 				self.instance.setPixmap(png)
-			elif self.defaultLogo != None:
-				self.instance.setPixmap(self.defaultLogo)
+			else:
+				defaultLogo = self.getDefaultLogo()
+				if defaultLogo != None:
+					self.instance.setPixmap(defaultLogo)
+
+	def getDefaultLogo(self):
+		if self.logoType == "model":
+			defaultPngName = resolveFilename(SCOPE_GUISKIN, "icons/logos/deflogo.svg")
+		else:
+			defaultPngName = resolveFilename(SCOPE_GUISKIN, "icons/logos/deflogo-small.svg")
+
+		is_svg = defaultPngName.endswith(".svg")
+		return LoadPixmap(defaultPngName, width=self.instance.size().width(), height=0 if is_svg else self.instance.size().height())
+	
+	def getLogoPngName(self):
+		logoname = ""
+		if self.logoType == "model":
+			return model
+		elif self.logoType == "brand":
+			if model in ("vuzero4k", "vusolo4k", "vuuno4kse", "vuduo4k", "vuduo4kse", "vuultimo4k"):
+				logoname = "vulogo"
+		return logoname
+	
