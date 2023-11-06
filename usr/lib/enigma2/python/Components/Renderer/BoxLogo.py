1a28a3e5c77d2f8b7316d3290fc35668ac6ecedb
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2023-11-06 20:53:28 +0200

    [Added] Bunch of logos for several brands and models

--- usr/lib/enigma2/python/Components/Renderer/BoxLogo.py
+++ usr/lib/enigma2/python/Components/Renderer/BoxLogo.py
@@ -16,10 +16,17 @@ def getLogoPngName(logoType):
 		if logoType == "model":
 			return model or ""
 		elif logoType == "brand":
-			if model and model in ("vuzero4k", "vusolo4k", "vuuno4kse", "vuduo4k", "vuduo4kse", "vuultimo4k"):
+			if model and model in ("vusolo", "vuduo", "vuuno", "vuzero", "vusolo2", "vusolose", "vuduo2",  "vuultimo", "vuzero4k", "vusolo4k", "vuuno4k", "vuuno4kse", "vuduo4k", "vuduo4kse", "vuultimo4k"):
 				logoname = "vulogo"
 			elif model and model in ("dagsmv200"):
-				logoname = "qviart"
+				logoname = "qviartlogo"
+			elif model and model in ("sf8008", "sf8008m", "sx988", "sx88v2", "sfx6008", "sfx6018"):
+				logoname = "octagonlogo"
+			elif model and model in ("pulse4k", "pulse4kmini"):
+				logoname = "pulselogo"
+			elif model and model in ("gbmv200"):
+				logoname = "gigabluelogo"
+
 		return logoname
 
 def getDefaultLogo(logoType, width, height):
